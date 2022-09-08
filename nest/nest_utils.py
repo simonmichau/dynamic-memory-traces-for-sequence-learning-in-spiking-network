de19ea000d5e642defe10593814af72f4f6e90da
simonmichau <simon.michau@rwth-aachen.de>
2022-09-08 12:22:08 +0200

    Added different readout modes for `measure_network`

--- nest/nest_utils.py
+++ nest/nest_utils.py
@@ -1,4 +1,5 @@
 import os
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import nest
@@ -57,19 +58,40 @@ def run_simulation(inpgen, t):
     nest.Simulate(t)
 
 
-def measure_node_collection(network, nc, inpgen=None, t_sim=5000.0, save_figures=False, readout_size=20):
+def measure_network(network, id_list: list = None, node_collection=None, readout_size: int = None,
+                            inpgen=None, t_sim: float = 5000.0, save_figures: bool = False):
     """
     Simulates given **NodeCollection** for **t_sim** and plots the recorded spikes, membrane potential and presented
     patterns. Requires an **InputGenerator** object for pattern input generation.
+
+    Readout modes (listed after priority):
+        1. "Watch List": a list of Node IDs is specified and measured. Useful when observing readout over several measurements
+        2. "Node Collection": A specified NodeCollection is measured
+        3. "Random k": k nodes are randomly sampled from the network and measured
+        4. "All": Measure all nodes in network
     """
-    # TODO: randomly sample [readout_size] neurons from Network and measure them (replace nc)
+    if id_list is not None:  # Readout Mode 1
+        node_collection = nest.NodeCollection(id_list)
+    elif node_collection is not None:  # Readout Mode 2
+        pass  # Do nothing
+    elif readout_size is not None:  # Readout Mode 3
+        global_ids = network.get_node_collections().get('global_id')
+        id_list = []
+        while len(id_list) < readout_size:
+            id_list.append(random.randrange(min(global_ids), max(global_ids)))
+            id_list = list(set(id_list))  # remove duplicates
+        id_list.sort()
+        node_collection = nest.NodeCollection(id_list)
+    else:  # Readout Mode 4
+        node_collection = network.get_node_collections()
+
     if network.multimeter is None:
         network.multimeter = nest.Create('multimeter')
         network.multimeter.set(record_from=['V_m'])
-        nest.Connect(network.multimeter, nc)
+        nest.Connect(network.multimeter, node_collection)
     if network.spikerecorder is None:
         network.spikerecorder = nest.Create('spike_recorder')
-        nest.Connect(nc, network.spikerecorder)
+        nest.Connect(node_collection, network.spikerecorder)
 
     if inpgen is None:
         nest.Simulate(t_sim)
@@ -94,7 +116,7 @@ def measure_node_collection(network, nc, inpgen=None, t_sim=5000.0, save_figures
     ts = dSD["times"]
 
     ax2.plot(ts, evs, ".", color='orange')
-    ax2.set_ylabel("Spikes")
+    ax2.set_ylabel("Spike ID")
 
     # PRESENTED PATTERNS
     time_shift = nest.biological_time - t_sim
@@ -111,6 +133,7 @@ def measure_node_collection(network, nc, inpgen=None, t_sim=5000.0, save_figures
     if save_figures:
         plt.savefig("simulation_%ds.png" % int(nest.biological_time/1000.0))
     plt.show()
+    return id_list
 
 
 def generate_nest_code(neuron_model: str, synapse_model: str, regen=True):
